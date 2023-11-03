1c7b38e28ed9661455adf0a7ddeeff6039dd90f8
RustamAleev2023 <ridermix@yandex.ru>
2023-11-03 09:25:36 +0200

    Added master branch

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,20 @@ public class Main {
 
         Aspirant aspirant  = new Aspirant("Research", "Petrov Petr", 30);
         System.out.println(aspirant);
+
+        Student student1 = new Student("Semenov Semen", 25);
+        System.out.println(student1);
+
+        Student  aspirant1 = new Aspirant("Research", "Stepanov Stepan", 34);
+
+        Ball redBall = new Ball("red");
+        Ball blackBall = new Ball("black");
+        Ball redBall1 = new Ball("red");
+        Ball blackBall1 = new Ball("black");
+
+        System.out.println(redBall.getCountInstance());
+        System.out.println(blackBall.getCountInstance());
+
+
     }
 }
